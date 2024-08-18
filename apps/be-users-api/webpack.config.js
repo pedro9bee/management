d9c4f65ada425ea54d4f08d51f1e1136013c7ba2
Pedro Lemos <pedro9bee@gmail.com>
2024-08-18 15:10:50 +0100

    fix: rename app to users-api to be-users-api

--- apps/be-users-api/webpack.config.js
+++ apps/be-users-api/webpack.config.js
@@ -3,7 +3,7 @@ const { join } = require('path');
 
 module.exports = {
   output: {
-    path: join(__dirname, '../../dist/apps/users-api'),
+    path: join(__dirname, '../../dist/apps/be-users-api'),
   },
   plugins: [
     new NxAppWebpackPlugin({
@@ -11,7 +11,7 @@ module.exports = {
       compiler: 'tsc',
       main: './src/main.ts',
       tsConfig: './tsconfig.app.json',
-      assets: ['./src/assets'],
+      assets: [join(__dirname, 'src/assets')],
       optimization: false,
       outputHashing: 'none',
     }),
